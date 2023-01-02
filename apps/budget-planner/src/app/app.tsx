f45099c02546a4d5117542c8e89255585c53518f
sebikap-gm2 <sebastian.kaplanski@gm2dev.com>
2023-01-02 20:04:25 -0300

    feat: remove unused nx code

--- apps/budget-planner/src/app/app.tsx
+++ apps/budget-planner/src/app/app.tsx
@@ -1,21 +1,14 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
-import NxWelcome from './nx-welcome';
 
 import { Route, Routes, Link } from 'react-router-dom';
 
 export function App() {
   return (
     <>
-      <NxWelcome title="budget-planner" />
-      <div />
-
       {/* START: routes */}
       {/* These routes and navigation have been generated for you */}
       {/* Feel free to move and update them to fit your needs */}
-      <br />
-      <hr />
-      <br />
       <div role="navigation">
         <ul>
           <li>
